c41042f116d1a1ca4d07aef91f067948fc8dea2b
tim <timscode1@gmail.com>
2018-11-29 17:57:46 -0500

    more avatar work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181129164005) do
+ActiveRecord::Schema.define(version: 20181129224551) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20181129164005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
